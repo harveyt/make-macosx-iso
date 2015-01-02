f1399992fddb7a615858b2aebf7e34acf4b0b0b6
Harvey Thompson <harveyandsu@me.com>
2015-01-01 16:25:29 -0800

    Added LICENSE and improved README. Script includes both.

--- README.md
+++ README.md
@@ -3,15 +3,35 @@ make-macosx-iso
 
 Create Mac OS X ISO image from App Store installation app.
 
-Supports:
+Supports
+--------
 
 * Mac OS X 10.9 "Mavericks"
 * Mac OS X 10.10 "Yosemite"
 
-Note that you *must* have already downloaded the OS installer in the App Store.
+NOTE: You *must* have already downloaded the OS installer in the App Store.
 
-For example:
+Example
+-------
 
 ```
 $ ./make-macosx-iso.sh -v Mavericks -o Mavericks-Install.iso
 ```
+
+Thanks
+------
+
+Thanks to information from:
+
+- http://forums.appleinsider.com/t/159955/howto-create-bootable-mavericks-iso
+- http://sqar.blogspot.de/2014/10/installing-yosemite-in-virtualbox.html
+- http://www.insanelymac.com/forum/topic/301988-how-to-create-a-bootable-yosemite-install-updated
+
+License
+=======
+
+Copyright (c) 2015 Harvey John Thompson.
+
+See [LICENSE] file for license rights and limitations (MIT).
+
+
--- make-macosx-iso.sh
+++ make-macosx-iso.sh
@@ -1,14 +1,59 @@
 #!/bin/bash
 #
-# Create Mac OS X ISO images.
-#
-# NOTE: Requires you to own a Mac and download installer manually in the App Store.
-#
-# Compiled using information from:
+# make-macosx-iso
+# ===============
+# 
+# Create Mac OS X ISO image from App Store installation app.
+# 
+# Supports
+# --------
+# 
+# * Mac OS X 10.9 "Mavericks"
+# * Mac OS X 10.10 "Yosemite"
+# 
+# NOTE: You *must* have already downloaded the OS installer in the App Store.
+# 
+# Example
+# -------
+# 
+# ```
+# $ ./make-macosx-iso.sh -v Mavericks -o Mavericks-Install.iso
+# ```
+# 
+# Thanks
+# ------
+# 
+# Thanks to information from:
+# 
 # - http://forums.appleinsider.com/t/159955/howto-create-bootable-mavericks-iso
 # - http://sqar.blogspot.de/2014/10/installing-yosemite-in-virtualbox.html
-# - http://www.insanelymac.com/forum/topic/301988-how-to-create-a-bootable-yosemite-install-updated/
+# - http://www.insanelymac.com/forum/topic/301988-how-to-create-a-bootable-yosemite-install-updated
+# 
+# License
+# =======
 #
+# Copyright (c) 2015 Harvey John Thompson
+# 
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+# 
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+
 DEFAULT_VERSION=Yosemite
 DEFAULT_INSTALL_APP=
 DEFAULT_OUTPUT_ISO=
